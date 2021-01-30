73228af556d310819a4b655fcbdb355dfcab0758
Hebx <lord.noctx@gmail.com>
2021-01-30 01:24:44 +0000

    create & style signin page, form-input and custom-button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from 'pages/homepage/homepage.component.jsx'
 import ShopPage from 'pages/shop/shop.component.jsx'
 import Header from 'components/header/header.component.jsx'
+import SignInAndSignUp from 'pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import {Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   )
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import FormInput from 'components/form-input/form-input.component.jsx'
+import CustomButton from 'components/custom-button/custom-button.component.jsx'
+import './sign-in.styles.scss'
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({email: '', password: ''})
+  }
+  handleChange = e => {
+    const {value, name} = e.target
+    this.setState({[name]: value})
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            label="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
