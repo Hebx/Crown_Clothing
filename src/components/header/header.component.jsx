b9730cdffa68f2f5f253d976687355a4f8f1d8ea
Hebx <lord.noctx@gmail.com>
2021-02-24 23:51:41 +0000

    added Redux and implemented userReducer & userActions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import './header.styles.scss'
 import {Link} from 'react-router-dom'
 import {ReactComponent as Logo} from 'assets/081 crown.svg'
 import {auth} from 'firebase/firebase.utils.js'
+import {connect} from 'react-redux'
 
 const Header = ({currentUser}) => {
   return (
@@ -31,4 +32,8 @@ const Header = ({currentUser}) => {
   )
 }
 
-export default Header
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(Header)
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
-
+import store from 'redux/store.js'
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 )
 
