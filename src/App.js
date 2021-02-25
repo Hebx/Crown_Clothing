8ebd82ac608d037d2ef70d22658773ba52c30a19
Hebx <lord.noctx@gmail.com>
2021-02-25 03:29:14 +0000

    adding redirect to home page for signed in user

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from 'pages/homepage/homepage.component.jsx'
 import ShopPage from 'pages/shop/shop.component.jsx'
 import Header from 'components/header/header.component.jsx'
 import SignInAndSignUp from 'pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import {auth, createUserProfileDocument} from 'firebase/firebase.utils.js'
 import {connect} from 'react-redux'
 import {setCurrentUser} from 'redux/user/user.actions.js'
@@ -37,14 +37,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Switch>
       </div>
     )
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser,
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
