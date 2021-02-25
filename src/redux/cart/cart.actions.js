40b46da50d5f59ae00a8fb0e99a39947aeb769e1
Hebx <lord.noctx@gmail.com>
2021-02-25 21:44:53 +0000

    Added cartIcon & cartDropdown components and Implemented cart reducer

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,4 @@
+import CartActionTypes from 'redux/cart/cart.types.js'
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+})
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import userReducer from 'redux/user/user.reducer.js'
+import cartReducer from 'redux/cart/cart.reducer.js'
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 })
